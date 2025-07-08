3ac3301a2938de0c446ae10bbb9d5248284a48d6
Marcos Hernandez <maalhequi95@gmail.com>
2025-07-08 15:39:27 -0500

    enable cors for admin panel

--- src/app.ts
+++ src/app.ts
@@ -54,7 +54,9 @@ export class Server {
         "https://motowork.xyz",
         "http://motowork.xyz",
         "http://motowork.co",
-        "https://motowork.co"
+        "https://motowork.co",
+        "http://admin.motowork.co/",
+        "https://admin.motowork.co/"
       ],
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       credentials: true,
